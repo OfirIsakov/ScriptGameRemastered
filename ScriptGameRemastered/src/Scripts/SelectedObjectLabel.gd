5cd11a1535fb1b23e4b065ed18be43b9e0c9f4be
Ofir Isakov <ofirfifi@gmail.com>
2021-05-11 19:21:30 +0300

    Change hierarchy of some items, combine sprites into sheets and make the game playable

--- ScriptGameRemastered/src/Scripts/SelectedObjectLabel.gd
+++ ScriptGameRemastered/src/Scripts/SelectedObjectLabel.gd
@@ -1,16 +1,12 @@
 extends Label
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rect_position = get_global_mouse_position() + Vector2(-530, -340) # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rect_position = get_global_mouse_position() + Vector2(-530, -340)
+	rect_position = get_global_mouse_position() + Vector2(-100, -400)
